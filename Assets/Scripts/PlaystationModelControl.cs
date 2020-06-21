fea7c72d3e932803e6dccd0bc85220816ed8b695
Bimane1900 <s.roysson@bahnhof.se>
2020-06-21 12:05:28 +0200

    Added particle effects when pressing buttons

--- Assets/Scripts/PlaystationModelControl.cs
+++ Assets/Scripts/PlaystationModelControl.cs
@@ -1,27 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.ParticleSystemModule;
 
 public class PlaystationModelControl : MonoBehaviour
 {
     [Header("Triangle")]
     public Light TriangleLight;
     public MeshRenderer TriangleRenderer;
+    public ParticleSystem TriangleParticles;
     Material TriangleMat;
+
 
     [Header("Square")]
     public Light SquareLight;
     public MeshRenderer SquareRenderer;
+    public ParticleSystem SquareParticles;
     Material SquareMat;
 
     [Header("Cross")]
     public Light CrossLight;
     public MeshRenderer CrossRenderer;
+    public ParticleSystem CrossParticles;
     Material CrossMat;
 
     [Header("Circle")]
     public Light CircleLight;
     public MeshRenderer CircleRenderer;
+    public ParticleSystem CircleParticles;
     Material CircleMat;
 
     // Start is called before the first frame update
@@ -36,6 +42,7 @@ public class PlaystationModelControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Triangle inputs management
         if(Input.GetButton("Triangle")){
             TriangleMat.EnableKeyword("_EMISSION");
             TriangleLight.intensity = 3;
@@ -44,6 +51,10 @@ public class PlaystationModelControl : MonoBehaviour
             TriangleMat.DisableKeyword("_EMISSION");
             TriangleLight.intensity = 0;
         }
+        if(Input.GetButtonDown("Triangle"))
+            TriangleParticles.Play();
+
+        //Circle inputs management
         if(Input.GetButton("Circle")){
             CircleMat.EnableKeyword("_EMISSION");
             CircleLight.intensity = 3;
@@ -52,6 +63,10 @@ public class PlaystationModelControl : MonoBehaviour
             CircleMat.DisableKeyword("_EMISSION");
             CircleLight.intensity = 0;
         }
+        if(Input.GetButtonDown("Circle"))
+            CircleParticles.Play();
+        
+        //Square inputs management
         if(Input.GetButton("Square")){
             SquareMat.EnableKeyword("_EMISSION");
             SquareLight.intensity = 3;
@@ -60,6 +75,10 @@ public class PlaystationModelControl : MonoBehaviour
             SquareMat.DisableKeyword("_EMISSION");
             SquareLight.intensity = 0;
         }
+        if(Input.GetButtonDown("Square"))
+            SquareParticles.Play();
+        
+        //Cross inputs management
         if(Input.GetButton("Cross")){
             CrossMat.EnableKeyword("_EMISSION");
             CrossLight.intensity = 3;
@@ -68,5 +87,7 @@ public class PlaystationModelControl : MonoBehaviour
             CrossMat.DisableKeyword("_EMISSION");
             CrossLight.intensity = 0;
         }
+        if(Input.GetButtonDown("Cross"))
+            CrossParticles.Play();
     }
 }
